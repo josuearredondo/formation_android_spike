d305695bbe8f62b421e321766c146d4b256d7f31
josuearredondo <josue.arredondo@hotmail.fr>
2016-09-28 14:22:08 +0200

    Created Palindrome UI

--- app/src/main/java/com/josue/spike/PalindromeActivity.java
+++ app/src/main/java/com/josue/spike/PalindromeActivity.java
@@ -13,5 +13,11 @@ public class PalindromeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palindrome);
+
+        /** Palindrome basic UI */
+        final EditText editText = (EditText) findViewById(R.id.input_edit_text);
+        final TextView textView = (TextView) findViewById(R.id.output_text_view);
+        final Button button = (Button) findViewById(R.id.button_ok);
+
     }
 }
