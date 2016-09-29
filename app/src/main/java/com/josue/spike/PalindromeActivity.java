d3ce6f5eeeadd900126c7f8e88b09298bf101957
josuearredondo <josue.arredondo@hotmail.fr>
2016-09-29 16:22:26 +0200

    Created listener method and circle textview green/red

--- app/src/main/java/com/josue/spike/PalindromeActivity.java
+++ app/src/main/java/com/josue/spike/PalindromeActivity.java
@@ -1,7 +1,13 @@
 package com.josue.spike;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,9 +20,28 @@ public class PalindromeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palindrome);
 
-        /** Palindrome basic UI */
+        /** Palindrome UI */
         final EditText editText = (EditText) findViewById(R.id.input_edit_text);
-        final TextView textView = (TextView) findViewById(R.id.output_text_view);
-        final Button button = (Button) findViewById(R.id.button_ok);
+        final TextView textView = (TextView) findViewById(R.id.circle_text);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Palindrome palindrome = new Palindrome();
+                if (palindrome.isPalindrome(editText.getText().toString())) {
+                    textView.setBackgroundResource(R.drawable.circle_text_green);
+                }else {
+                    textView.setBackgroundResource(R.drawable.circle_text_red);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
