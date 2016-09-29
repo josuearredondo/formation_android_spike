812ba46518d543e6fce132a4da1d5d033763c0b0
josuearredondo <josue.arredondo@hotmail.fr>
2016-09-29 09:46:29 +0200

    Created uni tests for Palindrome and correction of the Class

--- app/src/main/java/com/josue/spike/Palindrome.java
+++ app/src/main/java/com/josue/spike/Palindrome.java
@@ -9,30 +9,26 @@ import android.util.Log;
 public class Palindrome {
     /** Method to discover is a text is Palindrome */
     public boolean isPalindrome(String phrase) {
-        boolean bPalindrome = true;
+        boolean value=true;
+        int i,ind;
+        String cadena2="";
         /** Delete spaces in the start/end text */
-        StringBuilder sb = new StringBuilder(phrase);
-        while(sb.charAt(0)== ' ') {
-            sb.deleteCharAt(0);
+        for (int x=0; x < phrase.length(); x++) {
+            if (phrase.charAt(x) != ' ')
+                cadena2 += phrase.charAt(x);
         }
-        String resultSpacesStart = sb.toString();
-        StringBuilder sb2 = new StringBuilder(resultSpacesStart);
-        int endPalindrome = resultSpacesStart.length()-1;
-        while(sb2.charAt(endPalindrome)== ' ') {
-            sb2.deleteCharAt(endPalindrome);
-            endPalindrome =- endPalindrome;
-        }
-        String resultPalindrome = sb.toString();
-        int inc = 0;
-        int des = resultPalindrome.length()-1;
-        while ((inc<des)){
-            if (resultPalindrome.charAt(inc)==resultPalindrome.charAt(des)){
-                inc++;
-                des--;
-            } else {
-                bPalindrome = false;
+        /** Asign values again */
+        phrase=cadena2;
+        ind=phrase.length();
+        /** Compare phrase */
+        for (i= 0 ;i < (phrase.length()); i++){
+            if (phrase.substring(i, i+1).equals(phrase.substring(ind - 1, ind)) == false ) {
+                /** If one letter is not corresponded, the phrase is not palindrome and left from the for*/
+                value=false;
+                break;
             }
+            ind--;
         }
-        return bPalindrome;
+        return value;
     }
 }
--- app/src/test/java/com/josue/spike/ExampleUnitTest.java
+++ app/src/test/java/com/josue/spike/ExampleUnitTest.java
@@ -10,10 +10,22 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    /** First unit test for Palindrome */
+    /** First simple unit test for Palindrome */
     @Test
     public void wordIsPalindrome() throws Exception {
         Palindrome palindrome = new Palindrome();
-        assertEquals(true, palindrome.isPalindrome("  h1ouo1h"));
+        assertEquals(true, palindrome.isPalindrome("hooh"));
+    }
+    /** Unit test for Palindrome, with spaces */
+    @Test
+    public void wordIsPalindromeWithSpaces() throws Exception {
+        Palindrome palindrome = new Palindrome();
+        assertEquals(true, palindrome.isPalindrome(" ho oh "));
+    }
+    /** Unit test for Palindrome, with one letter in the middle*/
+    @Test
+    public void wordIsPalindromeLetterMiddle() throws Exception {
+        Palindrome palindrome = new Palindrome();
+        assertEquals(true, palindrome.isPalindrome("  h1ouo1h" ));
     }
 }
